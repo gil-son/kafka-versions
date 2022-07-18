f9aa550343f7e1de76815f58ef537ed6d692b364
gilszon <gilszon@gmail.com>
2022-07-18 10:12:54 -0300

    Organized

--- proje.../java-maven/basic-project2/alura-consumer/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ proje.../java-maven/basic-project2/alura-consumer/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -11,36 +11,42 @@ import java.util.Properties;
 public class FraudDetectorService {
 
     public static void main(String[] args){
-        var consumer = new KafkaConsumer<String, String>(new Properties());
+        var consumer = new KafkaConsumer<String, String>(properties());
         // List
         consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
 
         // Listening by time
-        var records = consumer.poll(Duration.ofMillis(100));
 
-        if(records.isEmpty()){
-            System.out.println(("Is empty"));
-        }
+        while(true) {
+            var records = consumer.poll(Duration.ofMillis(100));
 
-        for(var record : records){
-            System.out.println(("-----------------------------"));
-            System.out.println(("Processing new order, checking for fraud"));
-            System.out.println((record.key()));
-            System.out.println((record.value()));
-            System.out.println((record.partition()));
-            System.out.println((record.offset()));
+            if (records.isEmpty()) {
+                System.out.println(("Is empty"));
+                // return; // will stop the loop
+                continue;
+            }else{
+                System.out.println(("Record count: "+ records.count()));
+            }
 
-            // Simulation Fraud
+            for (var record : records) {
+                System.out.println(("-----------------------------"));
+                System.out.println(("Processing new order, checking for fraud"));
+                System.out.println((record.key()));
+                System.out.println((record.value()));
+                System.out.println((record.partition()));
+                System.out.println((record.offset()));
 
-            try{
-                Thread.sleep(5000);
-                System.out.println(("No fraud detected"));
-            } catch (InterruptedException exception) {
-                // ignoring
-                exception.printStackTrace();
+                // Simulation Fraud
+
+                try {
+                    Thread.sleep(5000);
+                    System.out.println(("No fraud detected"));
+                } catch (InterruptedException exception) {
+                    // ignoring
+                    exception.printStackTrace();
+                }
             }
         }
-
     }
 
     private static Properties properties(){
@@ -48,19 +54,15 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 
         // byte-to-String
-//        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-//        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+//        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         // Creating a Group to receive all requests from the producer
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName()); // getName
 
-//        properties.setProperty(ConsumerConfig);
-//        properties.setProperty(ConsumerConfig);
-//        properties.setProperty(ConsumerConfig);
-//        properties.setProperty(ConsumerConfig);
-
         return properties;
     }
+
 }
