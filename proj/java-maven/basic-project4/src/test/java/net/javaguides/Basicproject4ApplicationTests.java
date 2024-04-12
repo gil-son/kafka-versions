84c61eeb7b37be947e5a3e741b29535401cc52af
gil-son <gilszon@gmail.com>
2024-04-12 20:14:11 -0300

    Basic project 3

---
+++ proj/java-maven/basic-project4/src/test/java/net/javaguides/Basicproject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basicproject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
