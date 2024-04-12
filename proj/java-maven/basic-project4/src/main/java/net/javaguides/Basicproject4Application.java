4f49c44bf0c30271279b1f5a9d0c133d7bee3a91
gil-son <gilszon@gmail.com>
2024-04-12 20:16:40 -0300

    Basic project 4

---
+++ proj/java-maven/basic-project4/src/main/java/net/javaguides/Basicproject4Application.java
@@ -0,0 +1,13 @@
+package net.javaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basicproject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Basicproject4Application.class, args);
+	}
+
+}
