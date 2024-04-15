a2da8a4232f2817b5c2e665ec1be3ee7f5edc3f8
gil-son <gilszon@gmail.com>
2024-04-15 20:38:52 -0300

    Consumer

--- proj/java-maven/basic-project4/src/main/java/net/javaguides/kafka/KafkaConsumer.java
+++ proj/java-maven/basic-project4/src/main/java/net/javaguides/kafka/KafkaConsumer.java
@@ -2,21 +2,16 @@ package net.javaguides.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KafkaProducer {
+public class KafkaConsumer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    public void sendMessage(String message){
-        LOGGER.info(String.format("Message sent %s", message));
-        kafkaTemplate.send("my-topic", message);
+    @KafkaListener(topics = "my-topic", groupId="myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Message received -> %s", message));
     }
 }
