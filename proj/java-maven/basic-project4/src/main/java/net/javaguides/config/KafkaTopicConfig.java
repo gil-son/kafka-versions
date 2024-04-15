606f7337638871148a7a8f90c802ae30cf3a9387
gil-son <gilszon@gmail.com>
2024-04-15 19:39:06 -0300

    Configurations

---
+++ proj/java-maven/basic-project4/src/main/java/net/javaguides/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package net.javaguides.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic configTopic(){
+        return TopicBuilder.name("my-topic")
+                .partitions(10)
+                .build();
+    }
+}
