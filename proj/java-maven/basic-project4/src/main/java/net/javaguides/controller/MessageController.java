b0e309073365baec9fa1dd8967c5778bf6006a62
gil-son <gilszon@gmail.com>
2024-04-15 20:37:57 -0300

    Rest

---
+++ proj/java-maven/basic-project4/src/main/java/net/javaguides/controller/MessageController.java
@@ -0,0 +1,28 @@
+package net.javaguides.controller;
+
+import net.javaguides.kafka.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    private KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    // http:localhost:8080/api/v1/kafka/publish?message=hello world
+    @GetMapping("publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to the topic");
+    }
+
+
+}
---
+++ proj/java-maven/basic-project4/src/main/java/net/javaguides/kafka/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package net.javaguides.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent %s", message));
+        kafkaTemplate.send("my-topic", message);
+    }
+}
