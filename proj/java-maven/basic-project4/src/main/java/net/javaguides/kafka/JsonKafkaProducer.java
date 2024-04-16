675e4ce85b6069a0942effc204232d6cec64cf73
gil-son <gilszon@gmail.com>
2024-04-16 20:50:08 -0300

    JSON

--- proj/java-maven/basic-project4/src/main/java/net/javaguides/kafka/JsonKafkaProducer.java
+++ proj/java-maven/basic-project4/src/main/java/net/javaguides/kafka/JsonKafkaProducer.java
@@ -1,22 +1,32 @@
 package net.javaguides.kafka;
 
+import net.javaguides.payload.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KafkaProducer {
+public class JsonKafkaProducer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+    private KafkaTemplate<String, User> kafkaTemplate;
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String message){
-        LOGGER.info(String.format("Message sent %s", message));
-        kafkaTemplate.send("my-topic", message);
+    public void sendMessage(User data){
+        LOGGER.info(String.format("Message sent -> %s", data.toString()));
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                        .setHeader(KafkaHeaders.TOPIC, "my-topic")
+                                .build();
+
+        kafkaTemplate.send(message);
     }
 }
