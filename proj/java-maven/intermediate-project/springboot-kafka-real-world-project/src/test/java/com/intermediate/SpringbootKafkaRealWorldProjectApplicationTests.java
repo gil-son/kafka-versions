4eb0b235bde842e72cb3bc1fc928fa76515433f2
gil-son <gilszon@gmail.com>
2024-04-20 22:04:35 -0300

    Intermediate project

---
+++ proj/java-maven/intermediate-project/springboot-kafka-real-world-project/src/test/java/com/intermediate/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intermediate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
