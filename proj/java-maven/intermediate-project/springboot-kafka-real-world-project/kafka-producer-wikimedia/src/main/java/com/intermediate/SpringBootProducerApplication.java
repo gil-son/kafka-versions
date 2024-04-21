f18eff00ba7a5bf9569d49603eb492d5bc6e7c6e
gil-son <gilszon@gmail.com>
2024-04-20 22:03:50 -0300

    Created a module from springboot-kafka-real-worl-project (Parent)

---
+++ proj/java-maven/intermediate-project/springboot-kafka-real-world-project/kafka-producer-wikimedia/src/main/java/com/intermediate/SpringBootProducerApplication.java
@@ -0,0 +1,13 @@
+package com.intermediate;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+}
